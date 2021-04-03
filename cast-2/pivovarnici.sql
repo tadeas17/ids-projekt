146875f4cbea5850a6d9ed72daf0f9c7589c6c95
tadeas17 <39461727+tadeas17@users.noreply.github.com>
2021-04-03 03:21:24 +0200

    Update pivovarnici.sql
    vsechno malym pismem

--- cast-2/pivovarnici.sql
+++ cast-2/pivovarnici.sql
@@ -1,295 +1,308 @@
-create table "Prodejna"
+create table "prodejna"
 (
-    "ID_prodejny" NUMBER generated as identity
+    "id" NUMBER generated as identity
         constraint PRODEJNA_PK
             primary key,
-    "Nazev"       VARCHAR2(510) not null,
-    "Adresa"      VARCHAR2(510) not null
+    "nazev"       VARCHAR2(510) not null,
+    "adresa"      VARCHAR2(510) not null
 )
 /
 
-create table "Surovina"
+create table "surovina"
 (
-    "ID_suroviny" NUMBER generated as identity
+    "id" NUMBER generated as identity
         constraint SUROVINA_PK
             primary key,
-    "Nazev"       VARCHAR2(510) not null
+    "nazev"       VARCHAR2(510) not null
 )
 /
 
-create table "Surovina_prodejna_mnozstvi"
+create table "surovina_prodejna_mnozstvi"
 (
-    "ID_mnozstvi" NUMBER generated as identity
+    "id" NUMBER generated as identity
         constraint SUROVINA_PRODEJNA_MNOZSTVI_PK
             primary key,
     "mnozstvi[g]" NUMBER(12, 2),
-    "ID_prodejny" NUMBER
+    "id_prodejny" NUMBER
         constraint SUROVINA_PRODEJNA_MNOZSTVI_PRODEJNA_ID_PRODEJNY_FK
-            references "Prodejna"
+            references "prodejna"
                 on delete cascade,
-    "ID_suroviny" NUMBER not null
+    "id_suroviny" NUMBER not null
         constraint SUROVINA_PRODEJNA_MNOZSTVI_SUROVINA_ID_SUROVINY_FK
-            references "Surovina"
+            references "surovina"
                 on delete cascade
 )
 /
 
-create table HOSPODA
+create table "hospoda"
 (
-    ID     NUMBER not null
+    "id"     NUMBER not null
         primary key,
-    NAZEV  VARCHAR2(50),
-    ADRESA VARCHAR2(150)
+    "nazev"  VARCHAR2(50),
+    "adresa" VARCHAR2(150)
 )
 /
 
-create table PIVOVAR
+create table "pivovar"
 (
-    ID     NUMBER not null
+    "id"     NUMBER not null
         primary key,
-    NAZEV  VARCHAR2(50),
-    ADRESA VARCHAR2(150)
+    "nazev"  VARCHAR2(50),
+    "adresa" VARCHAR2(150)
 )
 /
 
-create table UZIVATEL
+create table "uzivatel"
 (
-    LOGIN         VARCHAR2(50) not null
+    "login"         VARCHAR2(50) not null
         primary key,
-    JMENO         VARCHAR2(50),
-    PRIJMENI      VARCHAR2(50),
-    TYP_UZIVATELE VARCHAR2(50) not null
-        check (TYP_UZIVATELE in ('admin', 'sladek', 'user')),
-    ID_PIVOVARU   NUMBER
+    "jmeno"         VARCHAR2(50),
+    "prijmeni"      VARCHAR2(50),
+    "typ_uzivatele" VARCHAR2(50) not null
+        check ("typ_uzivatele" in ('admin', 'sladek', 'user')),
+    "id_pivovar"   NUMBER
         constraint UZIVATEL_PIVOVAR_ID_FK
-            references PIVOVAR
+            references "pivovar"
                 on delete set null
 )
 /
 
-create table SLADKOVSKY_DIPLOM
+create table "sladkovsky_diplom"
 (
-    ID            NUMBER not null
+    "id"            NUMBER not null
         primary key,
-    DATUM_UDELENI DATE,
-    LOGIN         VARCHAR2(50)
+    "datum_udeleni" DATE,
+    "login_uzivatel"         VARCHAR2(50)
         constraint FK_DIPLOM_UZIVATEL
-            references UZIVATEL
+            references "uzivatel"
                 on delete cascade
 )
 /
 
-create table HODNOCENI_HOSPODY
+create table "hodnoceni_hospody"
 (
-    ID_HODNOCENI    NUMBER not null,
-    ID_HOSPODY      NUMBER not null
+    "id_hodnoceni"    NUMBER not null,
+    "id_hospody"      NUMBER not null
         constraint FK_HODNOCENI_ID_HOSPODY
-            references HOSPODA
+            references "hospoda"
                 on delete cascade,
-    DATUM           DATE,
-    HODNOTA         NUMBER,
-    KOMENTAR        VARCHAR2(500),
-    LOGIN_UZIVATELE VARCHAR2(50)
+    "datum"           DATE,
+    "hodnota"         NUMBER,
+    "komentar"        VARCHAR2(500),
+    "login_uzivatel" VARCHAR2(50)
         constraint FK_HODNOCENI_LOGIN_UZIVATELE
-            references UZIVATEL
+            references "uzivatel"
                 on delete cascade,
     constraint PK_HODNOCENI_HOSPODA
-        primary key (ID_HODNOCENI, ID_HOSPODY)
+        primary key ("id_hodnoceni", "id_hospody")
 )
 /
 
-create table RAMCOVA_SMLOUVA
+create table "ramcova_smlouva"
 (
-    ID             NUMBER not null
+    "id"             NUMBER not null
         primary key,
-    DATUM_UZAVRENI DATE,
-    DATUM_UKONCENI DATE,
-    SLEVA          NUMBER,
-    ID_HOSPODY     NUMBER
+    "datum_uzavreni" DATE,
+    "datum_ukonceni" DATE,
+    "sleva"          NUMBER,
+    "id_hospody"     NUMBER
         constraint FK_ID_HOSPODY
-            references HOSPODA
+            references "hospoda"
                 on delete cascade,
-    ID_PIVOVARU    NUMBER
+    "id_pivovaru"    NUMBER
         constraint FK_ID_PIVOVARU
-            references PIVOVAR
+            references "pivovar"
                 on delete cascade
 )
 /
 
-create table "Pivo"
+create table "pivo"
 (
-    "ID_pivo"        NUMBER generated as identity
+    "id"        NUMBER generated as identity
         constraint PIVO_PK
             primary key,
-    "Nazev"          VARCHAR2(255) not null,
-    "Barva"          NUMBER(2)
-        check ("Barva" BETWEEN 1 and 80),
-    "Typ"            VARCHAR2(16)
-        check ("Typ" in ('ale', 'stout', 'ipa', 'apa', 'red ipa')),
-    "Zpusob_kvaseni" VARCHAR2(16)
-        check ("Zpusob_kvaseni" in ('svrchně', 'spodně')),
-    "Obsah_alkoholu" NUMBER(2)     not null,
-    "ID_pivovaru"    NUMBER
+    "nazev"          VARCHAR2(255) not null,
+    "barva"          NUMBER(2)
+        check ("barva" BETWEEN 1 and 80),
+    "typ"            VARCHAR2(16)
+        check ("typ" in ('ale', 'stout', 'ipa', 'apa', 'red ipa')),
+    "zpusob_kvaseni" VARCHAR2(16)
+        check ("zpusob_kvaseni" in ('svrchně', 'spodně')),
+    "obsah_alkoholu" NUMBER(2)     not null,
+    "id_pivovaru"    NUMBER
         constraint PIVO_PIVOVAR_ID_FK
-            references PIVOVAR,
-    "ID_uzivatel"    VARCHAR2(50)
+            references "pivovar",
+    "login_uzivatel"    VARCHAR2(50)
         constraint PIVO_UZIVATEL_LOGIN_FK
-            references UZIVATEL
+            references "uzivatel"
 )
 /
 
-create table VARKA
+create table "varka"
 (
-    ID               NUMBER       not null
+    "id"               NUMBER not null
         constraint VARKA_PK
             primary key,
-    DATUM_VARENI     DATE,
-    "OBJEM[l]"       NUMBER(12, 2),
-    FORMA_DISTRIBUCE VARCHAR2(25),
-    CENA             NUMBER(12, 2),
-    "ID_pivo"        NUMBER       not null
+    "datum_vareni"     DATE,
+    "objem[l]"       NUMBER(12, 2),
+    "forma_distribuce" VARCHAR2(25),
+    "cena"             NUMBER(12, 2),
+    "id_pivo"        NUMBER not null
         constraint VARKA_PIVO_ID_PIVO_FK
-            references "Pivo"
-                on delete cascade,
-    "ID_uzivatel"    VARCHAR2(50) not null
-        constraint VARKA_UZIVATEL_LOGIN_FK
-            references UZIVATEL
-                on delete set null
-)
-/
-
-create table "Seznam_vypitych_piv"
-(
-    "ID_seznam_vypitych_piv"  NUMBER generated as identity
-        constraint SEZNAM_VYPITYCH_PIV_PK
-        primary key,
-    "Objem_vypiteho_piva[ml]" NUMBER default 0 not null,
-    "ID_pivo"                 NUMBER
-        constraint SEZNAM_VYPITYCH_PIV_PIVO_ID_PIVO_FK
-            references "Pivo"
-                on delete set null,
-    "ID_uzivatel"             VARCHAR2(50)
-        constraint SEZNAM_VYPITYCH_PIV_UZIVATEL_LOGIN_FK
-            references UZIVATEL
+            references "pivo"
                 on delete cascade
 )
 /
 
-create table "Surovina_pivo_mnozstvi"
+create table "seznam_vypitych_piv"
 (
-    "ID_mnozstvi" NUMBER generated as identity
+    "id"  NUMBER generated as identity
+        constraint SEZNAM_VYPITYCH_PIV_PK
+        primary key,
+    "objem_vypiteho_piva[ml]" NUMBER default 0 not null,
+    "id_pivo"                 NUMBER
+        constraint SEZNAM_VYPITYCH_PIV_PIVO_ID_PIVO_FK
+            references "pivo"
+                on delete set null,
+    "login_uzivatel"             VARCHAR2(50)
+        constraint SEZNAM_VYPITYCH_PIV_UZIVATEL_LOGIN_FK
+            references "uzivatel"
+                on delete cascade
+)
+/
+
+create table "surovina_pivo_mnozstvi"
+(
+    "id" NUMBER generated as identity
         constraint SUROVINA_PIVO_MNOZSTVI_PK
             primary key,
     "mnozstvi[g]" NUMBER(12, 2) not null,
-    "ID_suroviny" NUMBER
+    "id_suroviny" NUMBER
         constraint SUROVINA_PIVO_MNOZSTVI_SUROVINA_ID_SUROVINY_FK
-            references "Surovina"
+            references "surovina"
                 on delete cascade,
-    "ID_pivo"     NUMBER
+    "id_pivo"     NUMBER
         constraint SUROVINA_PIVO_MNOZSTVI_PIVO_ID_PIVO_FK
-            references "Pivo"
+            references "pivo"
                 on delete cascade
 )
 /
 
-create table "Objem_pivo_ramcova_smlouva"
+create table "objem_pivo_ramcova_smlouva"
 (
-    "ID_objem"           NUMBER generated as identity
+    "id"           NUMBER generated as identity
         constraint OBJEM_PIVO_RAMCOVA_SMLOUVA_PK
             primary key,
-    "ID_pivo"            NUMBER
+    "id_pivo"            NUMBER
         constraint OBJEM_PIVO_RAMCOVA_SMLOUVA_PIVO_ID_PIVO_FK
-            references "Pivo"
+            references "pivo"
                 on delete cascade,
-    "ID_ramcova_smlouva" NUMBER
+    "id_ramcova_smlouva" NUMBER
         constraint OBJEM_PIVO_RAMCOVA_SMLOUVA_RAMCOVA_SMLOUVA_ID_FK
-            references RAMCOVA_SMLOUVA
+            references "ramcova_smlouva"
                 on delete cascade,
     "objem[l]"           NUMBER
 )
 /
 
-create table "Slad"
+create table "slad"
 (
-    "ID_sladu"    NUMBER generated as identity
+    "id"    NUMBER generated as identity
         constraint SLAD_PK
             primary key,
-    BARVA         VARCHAR2(127),
-    "Puvod"       VARCHAR2(127),
-    "Extrakt"     VARCHAR2(127),
-    "ID_surovina" NUMBER not null
+    "barva"         VARCHAR2(127),
+    "puvod"       VARCHAR2(127),
+    "extrakt"     VARCHAR2(127),
+    "id_surovina" NUMBER not null
         constraint SLAD_SUROVINA_ID_SUROVINY_FK
-            references "Surovina"
+            references "surovina"
                 on delete cascade
 )
 /
 
-create table "Chmel"
+create table "chmel"
 (
-    "ID_chmelu"        NUMBER generated as identity
+    "id"        NUMBER generated as identity
         constraint CHMEL_PK
             primary key,
-    AROMA              VARCHAR2(255),
-    HORKOST            NUMBER,
-    PODIL_ALFA_KYSELIN NUMBER,
-    MISTO_PUVODU       VARCHAR2(255),
-    DOBA_SKLIZNE       DATE,
-    "ID_surovina"      NUMBER not null
+    "aroma"              VARCHAR2(255),
+    "horkost"            NUMBER,
+    "podil_alfa_kyselin" NUMBER,
+    "misto_puvodu"       VARCHAR2(255),
+    "doba_sklizne"       DATE,
+    "id_surovina"      NUMBER not null
         constraint CHMEL_SUROVINA_ID_SUROVINY_FK
-            references "Surovina"
+            references "surovina"
                 on delete cascade
 )
 /
 
-create table "Kvasnice"
+create table "kvasnice"
 (
-    "ID_kvasnic"  NUMBER generated as identity
+    "id"  NUMBER generated as identity
         constraint KVASNICE_PK
             primary key,
-    SKUPENSTVI    VARCHAR2(127),
-    MISTO_KVASENI VARCHAR2(255),
-    TYP           VARCHAR2(8)
-        check (typ in ('svrchni', 'spodni')),
-    "ID_surovina" NUMBER not null
+    "skupenstvi"    VARCHAR2(127),
+    "misto_kvaseni" VARCHAR2(255),
+    "typ"           VARCHAR2(8)
+        check ("typ" in ('svrchni', 'spodni')),
+    "id_surovina" NUMBER not null
         constraint KVASNICE_SUROVINA_ID_SUROVINY_FK
-            references "Surovina"
+            references "surovina"
                 on delete cascade
 )
 /
 
-create table "Hodnoceni_piva"
+create table "hodnoceni_piva"
 (
-    "ID_hodnoceni" NUMBER generated as identity
+    "id" NUMBER generated as identity
         constraint HODNOCENI_PIVA_PK
         primary key,
-    DATUM          TIMESTAMP(6) default current_date not null,
-    HODNOTA        NUMBER(1)                         not null
-        check (hodnota BETWEEN 1 and 5),
-    KOMENTAR       VARCHAR2(2047),
-    "ID_uzivatel"  VARCHAR2(50)                      not null
+    "datum"          TIMESTAMP(6) default current_date not null,
+    "hodnota"        NUMBER(1)                         not null
+        check ("hodnota" BETWEEN 1 and 5),
+    "komentar"       VARCHAR2(2047),
+    "id_uzivatel"  VARCHAR2(50)                      not null
         constraint HODNOCENI_PIVA_UZIVATEL_LOGIN_FK
-            references UZIVATEL
+            references "uzivatel"
                 on delete cascade,
-    "ID_pivo"      NUMBER                            not null
+    "id_pivo"      NUMBER                            not null
         constraint HODNOCENI_PIVA_PIVO_ID_PIVO_FK
-            references "Pivo"
+            references "pivo"
                 on delete cascade
 )
 /
 
-create table "Objem_hospoda_varka"
+create table "objem_hospoda_varka"
 (
-    "ID_objem"  NUMBER generated as identity
+    "id"  NUMBER generated as identity
         constraint OBJEM_HOSPODA_VARKA_PK
             primary key,
     "objem[l]"  NUMBER not null,
-    "ID_hopoda" NUMBER not null
+    "id_hopoda" NUMBER not null
         constraint OBJEM_HOSPODA_VARKA_HOSPODA_ID_FK
-            references HOSPODA
+            references "hospoda"
                 on delete cascade,
-    "ID_varka"  NUMBER not null
+    "id_varka"  NUMBER not null
         constraint OBJEM_HOSPODA_VARKA_VARKA_ID_FK
-            references VARKA
+            references "varka"
                 on delete cascade
 )
 /
+
+create table "varka_uzivatel"
+(
+    "id"       VARCHAR2(50) not null
+        constraint TABLE_NAME_UZIVATEL_LOGIN_FK
+            references "uzivatel"
+                on delete cascade,
+    "id_varka"          NUMBER       not null
+        constraint TABLE_NAME_VARKA_ID_FK
+            references "varka"
+                on delete cascade,
+    "id_varka_uzivatel" NUMBER generated as identity
+        constraint TABLE_NAME_PK
+            primary key
+)
+/
+
